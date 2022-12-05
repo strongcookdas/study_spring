ccec74f24aa12e218b5ebd950d6a0b944b16a735
strongcookdas <goodjav141592@gmail.com>
2022-12-05 17:03:24 +0900

    build : spring start

---
+++ spring_start/src/test/java/hello/hello_spring/SpringStartApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hello_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
